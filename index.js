08aae072961d7dfd04819f549f527f5a3f1b3d39
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-12-15 08:40:23 +0100

    setting up routes in the index file

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const connectDB = require('./config/db');
-const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes');
-const materialRoutes = require('./routes/materialRoutes');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const materialRoutes = require("./routes/materialRoutes");
+const examRoutes = require("./routes/examRoute");
+const resultRoutes = require("./routes/resultRoute");
+const uploadRoutes = require("./routes/uploadRoute");
 
 dotenv.config();
 connectDB();
-
 
 const app = express();
 
@@ -21,11 +23,11 @@ app.use(express.json());
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log('Server running on port', PORT);
+  console.log("Server running on port", PORT);
 });
 
-app.use('/api/materials', materialRoutes);
-app.use('/api/auth', authRoutes);
-
-
-
+app.use("/api/materials", materialRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/exam", examRoutes);
+app.use("/api/result", resultRoutes);
+app.use("/api/upload", uploadRoutes);
